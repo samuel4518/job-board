b21a88fd5ebfd4db7bd2a44864cd82e5974adf73
Hsin-En Lee <samuel4518@gmail.com>
2025-07-09 13:32:22 +0800

    feat: build main page

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel Job Board</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body class="mx-auto mt-10 max-w-2xl bg-slate-200 text-slate-700">
+    {{-- <div class="text-3xl bg-red-500"> TEST</div> --}}
+    {{ $slot }}
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,8 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('', fn() => to_route('jobs.index'));
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('jobs', JobController::class)->only(['index']);
